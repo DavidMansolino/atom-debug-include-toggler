7492cf1eda592a7ea5d788df2890395823bda990
DavidMansolino <David.Mansolino@cyberbotics.com>
2018-04-06 12:39:05 +0200

    Initial commit.

---
+++ spec/debug-include-toogler-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DebugIncludeTooglerView from '../lib/debug-include-toogler-view';
+
+describe('DebugIncludeTooglerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
